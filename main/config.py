bab9a8ac34078d2a208695136f5e3da75ddac62d
XIONG CHENGPENG <146049274+xxxcp@users.noreply.github.com>
2024-03-26 16:51:11 +0800

    Update config.py

--- main/config.py
+++ main/config.py
@@ -59,7 +59,7 @@ class BaseOptions(object):
 
         # * Training configs
         parser.add_argument("--bsz", type=int, default=64, help="mini-batch size")
-        parser.add_argument("--n_epoch", type=int, default=200, help="number of epochs to run")
+        parser.add_argument("--n_epoch", type=int, default=10, help="number of epochs to run")
         parser.add_argument("--max_es_cnt", type=int, default=200,
                             help="number of epochs to early stop, use -1 to disable early stop")
         parser.add_argument("--lr", type=float, default=1e-4, help="learning rate")
